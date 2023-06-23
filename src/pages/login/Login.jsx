a11b93a811fedb46d261a8a6a0eb287067caf590
anuragK-24 <24sept.anurag@gmail.com>
2023-06-24 00:30:04 +0530

    Changes in the Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     dispatch({type:"LOGIN_START"});       
     
     try {
-      const res = await axios.post("/auth/login",{
+      const res = await axios.post("https://blogapi-gpp7.onrender.com/api/auth/login",{
         username : userRef.current.value,
         password : passwordRef.current.value,
       })
