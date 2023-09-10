07ae0aa80f7a6706066a117a74e3860891fbdfc6
anuragK-24 <24sept.anurag@gmail.com>
2023-09-10 17:58:04 +0530

    Removed the error in the Post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -3,24 +3,18 @@ import {Link} from "react-router-dom"
 import { marked } from 'marked';
 
 export default function Post({post}) {
-  const PF = "https://blogapi-gpp7.onrender.com/images/"
-
+  
   return (
     <>
       <div className="post">
         <Link to={`/post/${post._id}`}  className="link">
-        { <img className='postImg' src={post.photo} alt="" />}
+          
+        {post.photo && post.photo.length!==0 && <img className='postImg' src={post.photo} alt="" />}
         <div className="postInfo">
-            <div className="postCats">
-              {post.categories.map((c)=>(
-                <span className="postCats">{c.name}</span>
-              ))}
-            </div>
               <span className="postTitle">{post.title}</span>
-
             <hr />
             <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
-                        {/* this line is used to display date in proper format  */}
+            {/* this line is used to display date in proper format  */}
         </div>
         <div className="postDesc"   dangerouslySetInnerHTML={{ __html: marked(post.desc) }} />
         {/* <p className="postDesc" >{post.desc}</p> */}
