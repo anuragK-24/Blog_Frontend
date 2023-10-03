3893c40d5b46ed234355eaed0633bd96652f418a
anuragK-24 <24sept.anurag@gmail.com>
2023-10-03 15:26:56 +0530

    Added the loader in the website

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,37 @@
-import { useState,useEffect } from 'react' 
-import Header from '../../components/header/Header'
-import Posts from '../../components/posts/Posts'
-import './home.css'
-import axios from 'axios'
-import { useLocation } from 'react-router-dom'
-
+import { useState, useEffect } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import loadingIcon from "../../image/loading.gif";
 
 export default function Home() {
-  const [posts,setPosts]  = useState([]);
-   const {search} = useLocation()
-  
+  const [posts, setPosts] = useState([]);
+  const [isResolved, setIsResolved] = useState(false);
+  const { search } = useLocation();
 
-  useEffect(()=>{
-    const fetchPosts = async () =>{
-      const res = await axios.get("https://blogapi-gpp7.onrender.com/api/posts" + search);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(
+        "https://blogapi-gpp7.onrender.com/api/posts" + search
+      );
+      setIsResolved(true);
       setPosts(res.data);
-    }
-    fetchPosts() 
-  },[search])
+    };
+    fetchPosts();
+  }, [search]);
   return (
-  <>
-    <Header/>
-    <div className="home">
-      <Posts posts={posts}/>
-    </div>
-  </>
-    
-  )
+    <>
+      <Header />
+
+      <div className="home">
+        {isResolved ? (
+          <img src={loading.gif} alt="" />
+        ) : (
+          <Posts posts={posts} />
+        )}
+      </div>
+    </>
+  );
 }
