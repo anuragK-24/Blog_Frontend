258f56ff820de031b92bde3fc344184d5b099219
anuragK-24 <24sept.anurag@gmail.com>
2023-10-03 17:00:47 +0530

    Added the animation in the searc box

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,7 +1,8 @@
 import "./topbar.css";
 import { Link } from "react-router-dom";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { Context } from "../../context/Context";
+import { useEffect } from "react";
 
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
@@ -10,6 +11,11 @@ export default function TopBar() {
     dispatch({ type: "LOGOUT" });
   };
   const [userPost, setUserPost] = useState("");
+
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div className="top">
@@ -77,6 +83,7 @@ export default function TopBar() {
         )}
         <Link to={`/?user=${userPost}`} className="link">
           <input
+            ref={inputRef}
             className="searchBox"
             type="text"
             onChange={(e) => {
@@ -85,7 +92,11 @@ export default function TopBar() {
           />
         </Link>
         <Link to={`/?user=${userPost}`} className="link">
-          <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+          <i
+          className={`topSearchIcon fa-solid fa-magnifying-glass ${
+            userPost.length > 0 ? 'active' : ''
+          } ${userPost.length > 0 ? 'shake' : ''}`}
+        ></i>
         </Link>
       </div>
     </div>
