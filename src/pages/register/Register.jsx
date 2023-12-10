3b40ffbf6100e8be9e68861f4d5c35edf8d4508d
anuragK-24 <24sept.anurag@gmail.com>
2023-12-11 01:45:58 +0530

    Added the change in register for navigation correctly

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,7 @@ import "./register.css"
 import { Link } from "react-router-dom"
 import { useState } from "react"
 import axios from "axios" 
-import { useNavigate } from 'react-router-dom';
+import { useHistory} from 'react-router-dom';
 
 export default function Register() {
   const [username,setUsername] = useState("")
@@ -10,7 +10,7 @@ export default function Register() {
   const [password,setPassword] = useState("")
   const [error,setError] = useState(false)
   
-  const navigate = useNavigate();
+  const history = useHistory();
   const handleSubmit = async (e) => {  //we are using async here cz it's just a fun not a useEffect
     e.preventDefault();
     setError(false);
@@ -20,7 +20,7 @@ export default function Register() {
         email,
         password,
       });
-      res.data && navigate('/login');
+      res.data && history.push('/login');;
       //if we don't have any error and if we have any data  it'll redirect to login page
     } catch (error) {
       setError(true);
