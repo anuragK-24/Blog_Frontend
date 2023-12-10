0d49203ef3627fb042075c47b57b3c312f7b0fdf
anuragK-24 <24sept.anurag@gmail.com>
2023-12-11 02:19:28 +0530

    Add Netlify redirects for client-side routing

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,26 +1,29 @@
-import "./register.css"
-import { Link } from "react-router-dom"
-import { useState } from "react"
-import axios from "axios" 
-import { useHistory} from 'react-router-dom';
+import "./register.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
-  const [username,setUsername] = useState("")
-  const [email,setEmail] = useState("")
-  const [password,setPassword] = useState("")
-  const [error,setError] = useState(false)
-  
-  const history = useHistory();
-  const handleSubmit = async (e) => {  //we are using async here cz it's just a fun not a useEffect
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    //we are using async here cz it's just a fun not a useEffect
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("https://blogapi-gpp7.onrender.com/api/auth/register",{
-        username,
-        email,
-        password,
-      });
-      res.data && history.push('/login');;
+      const res = await axios.post(
+        "https://blogapi-gpp7.onrender.com/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        }
+      );
+      res.data && navigate("/login");
       //if we don't have any error and if we have any data  it'll redirect to login page
     } catch (error) {
       setError(true);
@@ -28,20 +31,44 @@ export default function Register() {
   };
   return (
     <div className="register">
-        <span className="registerTitle">Register</span>
-      <form  className="registerForm" onSubmit={handleSubmit}>
+      <span className="registerTitle">Register</span>
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
-        <input type="text" className="registerInput " placeholder="Enter your name ..."  onChange={e=>setUsername(e.target.value.toLowerCase())}/>
+        <input
+          type="text"
+          className="registerInput "
+          placeholder="Enter your name ..."
+          onChange={(e) => setUsername(e.target.value.toLowerCase())}
+        />
         <label>Email</label>
-        <input type="text" className="registerInput " placeholder="Enter your email..." onChange={e=>setEmail(e.target.value.toLowerCase())}/>
+        <input
+          type="text"
+          className="registerInput "
+          placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value.toLowerCase())}
+        />
         <label>Password</label>
-        <input type="password" className="registerInput " placeholder="Enter your password..." onChange={e=>setPassword(e.target.value)}/>
-        <button className="registerButton" type="submit">Register</button>
+        <input
+          type="password"
+          className="registerInput "
+          placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="registerButton" type="submit">
+          Register
+        </button>
       </form>
       <button className="registerLoginButton">
-        <Link className="link" to="/login"> Login </Link>
+        <Link className="link" to="/login">
+          {" "}
+          Login{" "}
+        </Link>
       </button>
-      {error && <span style={{color:"yellow", marginTop:"15px", fontSize:"25px"}}>Something went wrong...</span>}
+      {error && (
+        <span style={{ color: "yellow", marginTop: "15px", fontSize: "25px" }}>
+          Something went wrong...
+        </span>
+      )}
     </div>
-  )
+  );
 }
