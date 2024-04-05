6e1e8e399936139b73545b39006d762804e2c3c5
anuragK-24 <24sept.anurag@gmail.com>
2024-04-05 12:19:17 +0530

    added the prediction feature from the available blog post

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useRef } from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/Header";
@@ -10,13 +12,16 @@ import { useContext } from "react";
 import { Context } from "../../context/Context";
 
 export default function Home() {
+  const [isFocused, setIsFocused] = useState(false);
   const { user } = useContext(Context);
-
+  const searchRef = useRef(null);
+  const searchItemsRef = useRef(null);
   const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
   const [isResolved, setIsResolved] = useState(false);
   const { search } = useLocation();
-  const [userPost, setUserPost] = useState("");
+  const [searchPost, setSearchPost] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -28,6 +33,8 @@ export default function Home() {
     };
     fetchPosts();
   }, [search]);
+
+  console.log("This is search blogs", searchPost);
   return (
     <>
       <Header />
@@ -35,7 +42,9 @@ export default function Home() {
       <div className="home">
         {!isResolved ? (
           <div className="home__loading">
-            <h2 className="home__loading__text">Fetching API at startup, please wait. Thanks for your patience!</h2>
+            <h2 className="home__loading__text">
+              Fetching API at startup, please wait. Thanks for your patience!
+            </h2>
             <img className="home__loading__icon" src={loadingIcon} alt="" />
           </div>
         ) : (
@@ -51,46 +60,83 @@ export default function Home() {
             ) : (
               ""
             )}
-            <div className="home__content__search">
+            <div
+              className="home__content__search"
+              style={{
+                borderBottomLeftRadius: isFocused ? "0px" : "10px",
+                borderBottomRightRadius: isFocused ? "0px" : "10px",
+              }}
+              onFocus={() => setIsFocused(true)}
+              onBlur={(e) => {
+                if (
+                  !searchRef.current.contains(e.relatedTarget) &&
+                  !searchItemsRef.current.contains(e.relatedTarget)
+                ) {
+                  setIsFocused(false);
+                }
+              }}
+            >
               <input
                 className="home__content__search__input"
-                placeholder="Search Blogs' by Author's name"
+                placeholder="Search Blogs"
                 type="text"
                 onChange={(e) => {
-                  setUserPost(e.target.value.toLowerCase());
+                  setInput(e.target.value);
+                  setSearchPost(e.target.value.toLowerCase());
                   const filteredPosts = posts.filter((post) => {
-                    return post.username
+                    return post.title
                       .toLowerCase()
-                      .includes(e.target.value.toLowerCase());
+                      .startsWith(e.target.value.toLowerCase());
                   });
-                  setPosts(filteredPosts);
 
-                  if (e.target.value.length > 0) {
-                    navigate(`/?user=${e.target.value.toLowerCase()}`);
-                  }
-                  if (e.target.value.length === 0) {
-                    navigate(`/`);
-                  }
+                  setSearchPost(filteredPosts);
                 }}
               />
-              <Link to={`/?user=${userPost}`} className="link">
+              <Link to={`/?user=${searchPost}`} className="link">
                 <i
                   className={`topSearchIcon fa-solid fa-magnifying-glass ${
-                    userPost.length > 0 ? "active" : ""
-                  } ${userPost.length > 0 ? "shake" : ""}`}
+                    searchPost.length > 0 ? "active" : ""
+                  } ${searchPost.length > 0 ? "shake" : ""}`}
                 ></i>
               </Link>
             </div>
-
+            {/* {isFocused && searchPost.length !== 0 ? (
+              <div className="home__content__searchItems">
+                {input.length !== 0 &&
+                  searchPost.map((post) => (
+                    <li
+                      onClick={() => navigate(`post/${post._id}`)}
+                      key={post._id}
+                    >
+                      {post.title}
+                    </li>
+                  ))}
+                {input.length !== 0 ? (
+                  <div>Search result : {searchPost.length}</div>
+                ) : (
+                  <div>Find your favourite blog</div>
+                )}
+              </div>
+            ) : null}{" "} */}
+            {isFocused && searchPost.length !== 0 && (
+              <div className="home__content__searchItems" ref={searchItemsRef}>
+                {input.length !== 0 &&
+                  searchPost.map((post) => (
+                    <li
+                      onClick={() =>{navigate(`post/${post._id}`)}}
+                      key={post._id}
+                    >
+                      {post.title}
+                    </li>
+                  ))}
+                {input.length !== 0 ? (
+                  <div>Search result : {searchPost.length}</div>
+                ) : (
+                  <div>Find your favourite blog post</div>
+                )}
+              </div>
+            )}
             <div className="home__content__posts">
-              {posts.length !== 0 ? (
-                <h3 className="text">
-                  {" "}
-                  Total {userPost}  Blogs : {posts.length}
-                </h3>
-              ) : (
-                ""
-              )}
               <Posts posts={posts} />
             </div>
           </div>
