93bff19d52bd50bc42cd1de4d973f83ff2ad9666
anuragK-24 <24sept.anurag@gmail.com>
2024-08-23 14:27:57 +0530

    Added the pagination concept and lazy loading and debouncing

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,14 +1,48 @@
-import  './posts.css'
-import Post from '../post/Post'
+import React, { useState, useEffect } from 'react';
+import './posts.css';
+import Post from '../post/Post';
+import axios from 'axios';
 
-export default function Posts({posts}) {
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchPosts = async (page) => {
+    try {
+      const res = await axios.get(
+        `https://blog-backend-zeta.vercel.app/api/posts?page=${page}&limit=3`
+      );
+      setPosts((prevPosts) => {
+        // Check if the new posts are already in the state
+        const newPosts = res.data.posts.filter(
+          (newPost) => !prevPosts.some((post) => post._id === newPost._id)
+        );
+        return [...prevPosts, ...newPosts];
+      });
+      setHasMore(res.data.hasMore);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts(page);
+  }, [page]);
+
   return (
+    <>
     <div className='posts'>
-      {posts.length===0 &&  <h1 className='text'>No Blog bound</h1>}
-      {
-      posts.map( (p,key) =>( 
-          <Post post={p} key={key}/>
+      {posts.length === 0 && <h1 className='text'>No Blog found</h1>}
+      {posts.map((p, key) => (
+        <Post post={p} key={key} />
       ))}
+      {hasMore && (
+          <button className='postsButton' onClick={() => setPage((prevPage) => prevPage + 1)}>
+            Load More
+          </button>
+        )}
     </div>
-  )
+    </>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,60 +12,91 @@ import { useContext } from "react";
 import { Context } from "../../context/Context";
 
 export default function Home() {
-  const [isFocused, setIsFocused] = useState(false);
   const { user } = useContext(Context);
+  const navigate = useNavigate();
+  const [searchPost, setSearchPost] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [debouncedSearchInput, setDebouncedSearchInput] = useState("");
   const searchRef = useRef(null);
   const searchItemsRef = useRef(null);
-  const navigate = useNavigate();
-  const [posts, setPosts] = useState([]);
-  const [isResolved, setIsResolved] = useState(false);
-  const { search } = useLocation();
-  const [searchPost, setSearchPost] = useState([]);
-  const [input, setInput] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const fetchPosts = async (searchQuery) => {
+    try {
+      const res = await axios.get(
+        `https://blog-backend-zeta.vercel.app/api/posts/search/${searchQuery}`
+      );
+      setSearchPost(res.data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await axios.get(
-        "https://blog-backend-zeta.vercel.app/api/posts" + search
-      );
-      setIsResolved(true);
-      setPosts(res.data);
-    };
-    fetchPosts();
-  }, [search]);
+    const handler = setTimeout(() => {
+      setDebouncedSearchInput(searchInput);
+    }, 500);
 
-  console.log("This is search blogs", searchPost);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchInput]);
+
+  useEffect(() => {
+    if (debouncedSearchInput) {
+      fetchPosts(debouncedSearchInput);
+    }
+  }, [debouncedSearchInput]);
+
   return (
     <>
       <Header />
 
       <div className="home">
-        {!isResolved ? (
+        {/* {!isResolved ? (
           <div className="home__loading">
             <h2 className="home__loading__text">
               Fetching API at startup, please wait. Thanks for your patience!
             </h2>
             <img className="home__loading__icon" src={loadingIcon} alt="" />
           </div>
-        ) : (
-          <div className="home__content">
-            {user ? (
-              <h2 className="greeting">
-                Welcome{" "}
-                <b>
-                  {" "}
-                  <i>{user.username.toUpperCase()}</i>{" "}
-                </b>{" "}
-              </h2>
-            ) : (
-              ""
-            )}
-            <div
-              className="home__content__search"
-              style={{
-                borderBottomLeftRadius: isFocused ? "0px" : "1em",
-                borderBottomRightRadius: isFocused ? "0px" : "1em",
+        ) : ( */}
+        <div className="home__content">
+          {user ? (
+            <h2 className="greeting">
+              Welcome{" "}
+              <b>
+                {" "}
+                <i>{user.username.toUpperCase()}</i>{" "}
+              </b>{" "}
+            </h2>
+          ) : (
+            ""
+          )}
+          <div
+            className="home__content__search"
+            style={{
+              borderBottomLeftRadius: isFocused ? "0px" : "1em",
+              borderBottomRightRadius: isFocused ? "0px" : "1em",
+            }}
+            onFocus={() => setIsFocused(true)}
+            onBlur={(e) => {
+              if (
+                !searchRef.current.contains(e.relatedTarget) &&
+                !searchItemsRef.current.contains(e.relatedTarget)
+              ) {
+                setIsFocused(false);
+              }
+            }}
+          >
+            <input
+              className="home__content__search__input"
+              placeholder="Search blogs..."
+              type="text"
+              onChange={(e) => {
+                setSearchInput(e.target.value);
               }}
+              value={searchInput}
               onFocus={() => setIsFocused(true)}
               onBlur={(e) => {
                 if (
@@ -75,54 +106,44 @@ export default function Home() {
                   setIsFocused(false);
                 }
               }}
-            >
-              <input
-                className="home__content__search__input"
-                placeholder="Search Blogs"
-                type="text"
-                onChange={(e) => {
-                  setInput(e.target.value);
-                  setSearchPost(e.target.value.toLowerCase());
-                  const filteredPosts = posts.filter((post) => {
-                    return post.title
-                      .toLowerCase()
-                      .startsWith(e.target.value.toLowerCase());
-                  });
-
-                  setSearchPost(filteredPosts);
-                }}
-              />
-              <Link to={`/?user=${searchPost}`} className="link">
-                <i
-                  className={`topSearchIcon fa-solid fa-magnifying-glass ${
-                    searchPost.length > 0 ? "active" : ""
-                  } ${searchPost.length > 0 ? "shake" : ""}`}
-                ></i>
-              </Link>
-            </div>
-            {isFocused && searchPost.length !== 0 && (
-              <div className="home__content__searchItems" ref={searchItemsRef}>
-                {input.length !== 0 &&
-                  searchPost.map((post) => (
+            />
+            <Link to={`/?user=${searchInput}`} className="link">
+              <i
+                className={`topSearchIcon fa-solid fa-magnifying-glass ${
+                  searchInput.length > 0 ? "active" : ""
+                } ${searchInput.length > 0 ? "shake" : ""}`}
+              ></i>
+            </Link>
+          </div>
+          {isFocused && (
+            <div className="home__content__searchItems" ref={searchItemsRef}>
+              {searchInput.length !== 0 && searchPost.length !== 0 ? (
+                <>
+                  {searchPost.map((post) => (
                     <li
-                      onClick={() =>{navigate(`post/${post._id}`)}}
+                      onClick={() => {
+                        navigate(`post/${post._id}`);
+                      }}
                       key={post._id}
                     >
                       {post.title}
                     </li>
                   ))}
-                {input.length !== 0 ? (
-                  <div>Search result : {searchPost.length}</div>
-                ) : (
+                  <div>Top Search results: {searchPost.length}</div>
+                </>
+              ) : searchInput.length !== 0 ? (
+                <div>No blog post found</div>
+              ) : (
+                <div>
                   <div>Find your favourite blog post</div>
-                )}
-              </div>
-            )}
-            <div className="home__content__posts">
-              <Posts posts={posts} />
+                </div>
+              )}
             </div>
+          )}
+          <div className="home__content__posts">
+            <Posts />
           </div>
-        )}
+        </div>
       </div>
     </>
   );
