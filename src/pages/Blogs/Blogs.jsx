ce480e35e0363ac33f9c2fe98711c8c867cd852e
anuragK-24 <24sept.anurag@gmail.com>
2025-08-16 01:59:06 +0530

    Change the positon of the topview component in the blogs component

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -5,16 +5,13 @@ import TopView from "../../components/TopView/TopView";
 export default function Blogs() {
   return (
     <div className="home">
-      {/* Sidebar */}
-      <div className="home__sidebar">
-        <TopView />
-      </div>
-
-      {/* Main content */}
       <div className="home__content">
         <div className="home__content__posts">
           <Posts />
         </div>
+      </div>
+      <div className="home__sidebar">
+        <TopView />
       </div>
     </div>
   );
